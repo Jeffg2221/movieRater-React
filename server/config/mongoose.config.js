de9e011fd6b07e9e0999ae7242a7d17d12727a17
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2022-12-31 15:35:30 -0500

    addded server update for mongoose

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = (DB) => {
     mongoose.set('strictQuery',false);
-    mongoose.connect('mongodb://0.0.0.0/' + DB)
+    mongoose.connect('mongodb://localhost/' + DB)
         .then(() => console.log(`Established a connection to the ${DB} database`))
         .catch(err => console.log(`Something went wrong when connecting to the ${DB} database `, err));
 
